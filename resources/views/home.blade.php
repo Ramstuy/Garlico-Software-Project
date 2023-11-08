b20e171dee7d4f5a91c0a96bc2c936a608625030
Ramstuy <ikrhidayat@gmail.com>
2023-11-08 23:10:04 +0800

    Added Landing page (first page)

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+
+@section('container')
+    <section class="firstbg">
+        <img src="https://c.animaapp.com/OU2pd5a9/img/rectangle-34.png" alt="garlicofirstbg">
+        <div class="h-100 container-xxl position-relative">
+            <div class="d-flex h-100 align-items-center">
+                <div class="text-white">
+                    <h1 class="firstle">DISCOVER OUR PRODUCT</h1>
+                    <p class="firstpar">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor /n incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut</p>
+                    <a href="#" class="firstbtn mt-2 btn btn-light p-3" role="button">Read More</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        
+    </section>
+@endsection
